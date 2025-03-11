8963cf4f80fb630724bd9e4e94107d14a31a136b
Manoj <mkv336699@gmail.com>
2025-03-11 19:02:04 +0530

    Prisma Setup

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import cors from 'cors';
+import { prismaClient } from './lib/db';
+// import prismaClient from './lib/db'
 
 async function init() {
     const app = express();
-    const PORT = 3000;
+    const PORT = Number(process.env.SERVER_PORT) || 3000;
 
     // Create GraphQL Server
     const gqlServer = new ApolloServer<any>({
@@ -14,11 +16,24 @@ async function init() {
                 hello: String,
                 say(name: String): String
             }
+            type Mutation {
+                createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+            }
         `,
         resolvers: {
             Query: {
                 hello: () => "{ success: true }",
                 say: (_, {name}: {name: String}) => name,
+            },
+            Mutation: {
+                createUser: async(_, { firstName, lastName, email, password }: { lastName: string, firstName: string, email: string, password: string }) => {
+                    await prismaClient.user.create({
+                        data: {
+                            firstName, lastName, email, password, salt: 'random'
+                        }
+                    })
+                    return true
+                }
             }
         }
     })
